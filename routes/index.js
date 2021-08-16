cdeab25fd96681488898ca25e0bef0066170e43e
Jason-Peralta <jperalta3197@gmail.com>
2021-08-16 15:42:58 -0400

    Adding handling of relational operator.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('C:\\Users\\jpera\\WebstormProjects\\weatherapi\\views\\index.html');
+  res.sendFile(__dirname + '/route_html/index.html');
 });
 
 module.exports = router;
